692ae4848a86af02d345a10b3d9a7b2c29d7d788
adamsbola <adamadu52@gmail.com>
2023-01-11 00:26:43 +0100

    ok

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+""" Working with Files and JSON  """
 import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def main():
+    """ store input args in JSON obj """
+    # make sure file exists
+    try:
+        with open("add_item.json", "x", encoding="utf-8"):
+            pass
+    except FileExistsError as e:
+        pass
+    finally:
+        # file exist, make sure line valid json ob
+        with open("add_item.json", "r+") as f:
+            if len(f.readline()) == 0:
+                f.write('[]')
+        # file exist, contains valid json
+        if len(sys.argv) == 1:
+            return
+        # get obj
+        arr = load_from_json_file("add_item.json")
+        for i, arg in enumerate(sys.argv):
+            if i == 0:
+                continue
+            arr.append(arg)
+        # save array
+        save_to_json_file(arr, "add_item.json")
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    main()
